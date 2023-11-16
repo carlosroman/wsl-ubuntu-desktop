bdc3a7038e6ff0e67d84f50e400410f2490cb310
Carlos Roman <carlos@cliche-corp.co.uk>
2023-11-16 17:59:00 +0000

    feat(setup): Fixed apt setup (python-venv)

--- scripts/apt_setup.sh
+++ scripts/apt_setup.sh
@@ -2,4 +2,4 @@
 set -e
 
 apt-get update
-apt install gcc autoconf make python3.10-venv libffi-dev --no-install-recommends
+apt install gcc autoconf make python3-venv libffi-dev --no-install-recommends
